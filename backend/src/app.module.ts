527fd943c2c974a5c7e8a38020abdbbbc401a84b
yuche <mick930203@gmail.com>
2026-08-06 15:47:14 +0800

    chore: init NestJS backend with Prisma and Redis

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { RedisModule } from '@nestjs-modules/ioredis';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    RedisModule.forRoot({
+        type: 'single',
+        url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
